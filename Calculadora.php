4d3316631c7cbde85c0278a2d5ea13a8bb6e6b59
Bruno <62807696+Wick096@users.noreply.github.com>
2021-04-22 18:31:14 -0300

    Add files via upload

--- Calculadora.php
+++ Calculadora.php
@@ -1,29 +1,73 @@
-
 <?php
-    if(isset($_GET["num1"])) {
-        $x = $_GET["num1"];
-        $y = $_GET["num2"];
-        $op = $_GET["operacao"];
+   
+    $n1=$_POST["n1"];
+    $n2=$_POST["n2"];
+    $op=$_POST["op"];
+    switch($op)
+    {
+        case "soma":
+            $resultado=$n1+$n2;
+           
+            echo "<script>alert('Resultado: $resultado'); location= './index.html';</script>";
+        break;
+        
+        case "sub":
+            $resultado=$n1-$n2;
+            echo "<script>alert('Resultado: $resultado'); location= './index.html';</script>";
 
-    if($op =="soma")
-        {
-            $z = $x+$y;
-            echo "$x + $y = $z";
-        }
-        elseif($op =="subtracao")
-        {
-            $z = $x-$y;
-            echo "$x - $y = $z";
-        }
-        elseif($op=="multiplicacao")
-        {   $z=$x*$y;
-            echo "$x*$y= $z";
-        }
-        else
-        {    
-            $z=$x/$y;
-            echo "$x/$y = $z";
-        }  
+        break;    
+
+        case "mult":
+            $resultado=$n1*$n2;
+            echo "<script>alert('Resultado: $resultado'); location= './index.html';</script>";
+
+        break;    
+
+        
+
+        case "potencia":
+            $resultado=pow($n1,$n2);
+            echo "<script>alert('Resultado: $resultado'); location= './index.html';</script>";
+
+        break;   
+
+        case "raiz":
+            $resultado=sqrt($n1);
+            echo "<script>alert('Resultado: $resultado'); location= './index.html';</script>";
+
+        break;   
+
+        case "um":
+            $resultado=(1/$n1);
+            echo "<script>alert('Resultado: $resultado'); location= './index.html';</script>";
+
+        break;   
+
+        case "porc":
+            $resultado=($n1/100*$n2);
+            echo "<script>alert('Resultado: $resultado'); location= './index.html';</script>";
+
+        break;   
+
     
+
+		}
+
+   if( $op == "div" ) {
+		
+		if ( $n2 != 0 ) {
+
+			$resultado = ($n1/ $n2);
+
+		} else {
+
+			echo "<script>alert('Não é possivel dividir: $resultado'); location= './index.html';</script>";
+			return 0;
+
+        
+        
+        }   
+
     }
+    
 ?>
